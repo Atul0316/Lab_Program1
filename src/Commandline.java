7f71a4d77dbde3bdc02a84604a5f9497fa9a356e
Aul1603 <asadakamol@gmail.com>
2023-11-03 14:21:06 +0530

    Initial commit

---
+++ src/Commandline.java
@@ -0,0 +1,12 @@
+
+
+public class Commandline {
+    public static void main(String[] args) {
+        float x= Float.parseFloat(args[0]);
+        float y=Float.parseFloat(args[1]);
+        float z=x+y;
+        System.out.println("Addition Result:"+z);
+
+
+    }
+}
